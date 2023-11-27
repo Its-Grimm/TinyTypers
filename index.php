17377daafbb4d5281a1cb6f142df5d5992ea623b
Grimm <witwityler@gmail.com>
2023-11-27 00:46:36 -0800

    successfully merged fileReader.php and index.html into index.php, making random text and the typetest js code work together with the html/css

--- index.php
+++ index.php
@@ -1,24 +1,62 @@
+<?php
+   $fileExists = 'englishWords.txt';
+
+   if (file_exists($fileExists)) {
+      $allWords = file($fileExists);
+   }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
    <title> Tiny Typers </title>
-   <link rel="stylesheet" href="syles.css">
+   <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
-   <!-- <img src="./Logos/svgs/TinyTypersLogoV1SVG.svg" alt="" width="307.5px" height="96px"> -->
+   <header>
+      <div class="leftMenus">
+         <ul>
+            <li> profile </li>
+            <li> friends </li>
+         </ul>
+      </div>
 
-   <textarea id='input_area' onfocus='startGame()' oninput='processText()'
-      placeholder="start typing here..."></textarea>
-   <br>
-   <label> WPM </label>
-   <label id='wpm'></label>
+      <div class="title">
+         <label> TinyTypers </label>
+      </div>
 
-   <label> Time </label>
-   <label id='time'>30s</label> 
-   
+      <div class="rightMenus">
 
+      </div>
+
+   </header>
+
+   <div class="aboveBoxSettings">
+      <nav>
+         <ul>
+            <li> Setting 1 </li>
+            <li> Setting 2 </li>
+            <li> Setting 3 </li>
+            <li> Setting 4 </li>
+            <li> Setting 5 </li>
+         </ul>
+      </nav>
+   </div>
+
+   <div class="main">
+      <label for="main" id="quote"> Start typing to start game </label>
+   </div>
+
+   <div class="underBoxSettings">
+      <ul>
+         <li class="wpmCounter" id="wpm"> wpm </li>
+         <li class="typeBox"> <input type="text" class="typeBox" id="input_area" placeholder="start typing here..."
+               onfocus='startGame()' oninput='processText()'> </li>
+         <li class="timer" id="time"> 30s </li>
+      </ul>
+   </div>
 
    <label> Errors </label>
    <label id='errors'>0</label>
@@ -26,60 +64,78 @@
    <label> % Accuracy </label>
    <label id='accuracy'>100</label>
    <button id='restart_btn' onclick='reset()'>Restart</button>
-   
-   <br><br>
-   <label id='quote'>Click on the area below to start the game.</label>
+
+   <footer>
+      <nav>
+         <ul>
+            <li> 1 </li>
+            <li> 2 </li>
+            <li> 3 </li>
+            <li> 4 </li>
+            <li> 5 </li>
+         </ul>
+      </nav>
+
+   </footer>
+
 
    <script>
+      // Grabs all of the elements from the html
       let wpm_text = document.getElementById('wpm');
       let errors_text = document.getElementById('errors');
       let accuracy_text = document.getElementById('accuracy');
       let timer_text = document.getElementById('time');
       let input_area = document.getElementById('input_area');
       let words = document.getElementById('quote');
+
       let timer = null;
-      let max_TIME = 20;
+      let max_TIME = 30;
       let time = max_TIME;
       let charTyped = 0;
       let total_errors = 0;
       let curr_words = "";
 
+      //Starts game
       function startGame() {
          reset();
          updateWords();
          clearInterval(timer);
          timer = setInterval(updateTimer, 1000);
-
       }
-      function reset() {
 
+      //Resets the game when restart button is clicked
+      function reset() {
          input_area.disabled = false;
          time = max_TIME;
          wpm.innerText = "";
-         timer_text.innerText = max_TIME;
+         timer_text.innerText = max_TIME + "s";
          input_area.value = "";
          accuracy_text.innerText = 100;
       }
-      function updateTimer() {
 
+      //Counts down timer, and finishes gaem if timer hits 0
+      function updateTimer() {
          if (time > 0) {
             time--;
             timer_text.innerText = time + 's';
-         } else {
+         }
+         else {
             finishGame();
          }
       }
+
+      //Finishes game, clears text, resets variables
       function finishGame() {
          clearInterval(timer);
          input_area.disabled = true;
          words.innerText = 'Click on restart to start new game.';
 
          let wpm = ((charTyped / 5) / max_TIME) * 60;
-         wpm_text.innerText = Math.round(wpm);
-
+         wpm_text.innerText = Math.round(wpm) + "wpm";
       }
-      function processText() {
 
+      //Handles text input, correct/incorrect text, moving cursor
+      function processText() {
          input = input_area.value;
          input_array = input.split('');
          words_array = words.querySelectorAll('span');
@@ -93,23 +149,23 @@
                if (char === words_char.innerText) {
                   words_char.classList.add('correct_char');
                   words_char.classList.remove('incorrect_char');
-               } else {
+               }
+               else {
                   words_char.classList.add('incorrect_char');
                   words_char.classList.remove('correct_char');
-                  if (index === input_array.length - 1)
+                  if (index === input_array.length - 1) {
                      errors++;
+                  }
                   if (words_char.innerText === " ") {
-
                      const charSpan = document.createElement('span');
                      charSpan.innerText = char;
                      words.insertBefore(charSpan, words_char);
                      charSpan.classList.add("extra_char");
-
                   }
                }
             }
-
          });
+
          total_errors = total_errors + errors;
          errors_text.innerText = total_errors;
 
@@ -117,13 +173,37 @@
          accuracy_text.innerText = Math.round(accuracy);
 
          if (input_array.length === words_array.length) {
-
             updateWords();
             input_area.value = "";
          }
       }
+
+      //Grabs words from the PHP above html
+      <?php
+         for ($i = 0; $i < 30; $i++) {
+            $randomIndex = rand(0, count($allWords) - 1);
+            $randomWords[$i] = $allWords[$randomIndex];
+         }
+
+         $firstWord = true;
+         $allWordsStr = "";
+         foreach ($randomWords as $word) {
+            $trimmedWord = rtrim($word, "\r\n");
+            if (!$firstWord) {
+               $allWordsStr .= " ";
+            } 
+            else {
+               $firstWord = false;
+            }
+            $allWordsStr .= $trimmedWord;
+         }
+      ?>
+
+      //WHERE TYPING WORDS ARE ASSIGNED
       function updateWords() {
-         curr_words = 'test your typing with tiny typers';
+         var arrayData = <?php echo json_encode($allWordsStr); ?>;
+         curr_words = arrayData;
+
          words.textContent = null;
 
          curr_words.split('').forEach(char => {
@@ -131,8 +211,8 @@
             charSpan.innerText = char;
             words.appendChild(charSpan);
          });
-
       }
+
    </script>
 </body>
 
