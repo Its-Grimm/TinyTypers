989177f22e4711b389b490c73fb82d332444710c
T <witiwtyler@gmail.com>
2023-11-27 16:04:21 -0800

    Added top bar and footer from index.php

--- friends.php
+++ friends.php
@@ -1,5 +1,32 @@
 <?php
-   
+
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+   <title>TinyType Friends</title>
+   <link rel="stylesheet" href="friends_styles.css">
+</head>
+
+<body>
+   <div id="wrapper">
+      <header>
+         <div class="title">
+            <label> TinyTypers </label>
+         </div>
+      </header>   
+
+      
+
+
+
+
+
+   </div>
+</body>
+
+</html>
