a68e3273a6c77ab36845745be5ef8148115135f0
T <witiwtyler@gmail.com>
2023-11-29 15:10:34 -0800

    created profile.php and profile_styles.css with same layout/css as friends page

---
+++ profile.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   <title> TinyTypers Profile </title>
+   <link rel="stylesheet" href="profile_styles.css">
+
+</head>
+
+
+<body>
+   <div id="wrapper">
+
+      <!-- HEADER -->
+
+      <header>
+         <div class="title">
+            <label> TinyTypers </label>
+         </div>
+      </header>
+
+      <!-- MAIN AREA -->
+
+      <div class="main">
+
+
+      </div>
+
+      <!-- FOOTER -->
+
+      <footer>
+         <nav>
+            <ul>
+               <li> 1 </li>
+               <li> 2 </li>
+               <li> 3 </li>
+               <li> 4 </li>
+               <li> 5 </li>
+            </ul>
+         </nav>
+
+      </footer>
+      
+   </div>
+
+
+</body>
+
+</html>
