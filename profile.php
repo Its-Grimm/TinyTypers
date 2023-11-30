a7a19b4ff16bbd6e50ab21200d84ebbf1f3890fa
T <witiwtyler@gmail.com>
2023-11-30 09:57:48 -0800

    added database connection, failing currently to connect

--- profile.php
+++ profile.php
@@ -4,45 +4,67 @@
 <head>
    <title> TinyTypers Profile </title>
    <link rel="stylesheet" href="profile_styles.css">
-
 </head>
 
 
 <body>
-   <div id="wrapper">
+<?php
 
-      <!-- HEADER -->
+// $db = false; //mysqli_connect("69.172.204.200", "tp", "New_Web_2023!", "tp_TinyTypers");
+// if (!$db) {
+//    echo "Cannot connect to database! Error: " ;
+//    echo "Something strange is going on if this message doesn't appear";
+//    //die("Cannot connect to database! Error: " . mysqli_connect_error());
+// } 
+// else {
+//    echo "IT WORKS";
+// }
 
-      <header>
-         <div class="title">
-            <label> TinyTypers </label>
+echo "before connect";
+$db = mysqli_connect("69.172.204.200", "tp", "New_Web_2023!", "tp_db");
+echo "after connect";
+if (!$db) {
+   die("Cannot connect to database! Error: " . mysqli_connect_error());
+}
+
+?>
+
+
+   <!-- <form name="loginForm" action="" method="post"> -->
+      
+      <div id="wrapper">
+
+         <!-- HEADER -->
+
+         <header>
+            <div class="title">
+               <label> TinyTypers </label>
+            </div>
+         </header>
+
+         <!-- MAIN AREA -->
+         <div class="main">
+            <label> HELLO </label>
+
          </div>
-      </header>
 
-      <!-- MAIN AREA -->
+         <!-- FOOTER -->
 
-      <div class="main">
-
+         <footer>
+            <nav>
+               <ul>
+                  <li> 1 </li>
+                  <li> 2 </li>
+                  <li> 3 </li>
+                  <li> 4 </li>
+                  <li> 5 </li>
+               </ul>
+            </nav>
+         </footer>
 
       </div>
 
-      <!-- FOOTER -->
-
-      <footer>
-         <nav>
-            <ul>
-               <li> 1 </li>
-               <li> 2 </li>
-               <li> 3 </li>
-               <li> 4 </li>
-               <li> 5 </li>
-            </ul>
-         </nav>
-
-      </footer>
-      
-   </div>
-
+   <!-- </form> -->
 
 </body>
 
