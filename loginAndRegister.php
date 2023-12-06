56a2696f866d530e19d5fd27446b9f98229ce1a2
T <witiwtyler@gmail.com>
2023-12-06 10:36:19 -0800

    changed from profile to loginAndRegister... for better naming and separate profile page

---
+++ loginAndRegister.php
@@ -0,0 +1,156 @@
+<?php
+// Login creds
+$loginUsernameOrEmail = filter_input(INPUT_POST, 'loginUserOrEmail');
+$loginPassword = filter_input(INPUT_POST, 'loginPass');
+// Register creds
+$regUsername = filter_input(INPUT_POST, 'regUser');
+$regEmail = filter_input(INPUT_POST, 'regEmail');
+$regPassword = filter_input(INPUT_POST, 'regPass');
+$regFName = filter_input(INPUT_POST, 'regFName');
+$regLName = filter_input(INPUT_POST, 'regLName');
+
+$db = mysqli_connect("69.172.204.200", "tp", "New_Web_2023!", "tp_Tiny_Typers");
+if (!$db) {
+   die("Cannot connect to database! Error: " . mysqli_connect_error());
+}
+
+// If user is registering
+if (isset($_POST['registerButton'])) {
+   $regUserQuery = "INSERT INTO user VALUES (null, '$regUsername', '$regEmail', '$regPassword', '54321')";
+   $userQueryResult = mysqli_query($db, $regUserQuery);
+   
+   if ($userQueryResult == false || $userQueryResult == null || $userQueryResult == "") {
+      die("User not added");
+   }
+   else{
+      header("Location: loginAndRegister.php");
+   }
+}
+
+// If user is logging in
+if (isset($_POST['loginButton'])) {
+   // Grabbing the password from the database thats associated with the username/email entered
+   $loginQuery = "SELECT password FROM user WHERE username = '$loginUsernameOrEmail' OR email = '$loginUsernameOrEmail'";
+   $queryResult = mysqli_query($db, $loginQuery);
+
+   if ($queryResult == '' || $queryResult == null) {
+      die("Invalid Username, Email, or Password entered. Try again!");
+   }
+   
+   if ($queryResult == false){
+      die("No user under those credentials found. Please try again!");
+   }
+   
+   $correctRow = mysqli_fetch_assoc($queryResult);
+   $fetchedPassword = $correctRow["password"];
+
+   // If the password entered in the textbox matches the one uploaded to the website
+   if ($loginPassword == $fetchedPassword){
+      // $_SESSION["loggedIn"] = "isLoggedIn";
+      $cookieName = "isLoggedIn";
+      $cookieValue = true;
+      setcookie($cookieName, $cookieValue, time() + (86400*30), "/");
+      header("Location: index.php");
+   }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+   <title> TinyTypers Login/Register </title>
+   <link rel="stylesheet" href="loginAndRegister_styles.css">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+
+<body>
+   <script src="loginAndRegisterJQuery.js"> </script>
+
+   <div id="wrapper">
+
+      <!-- HEADER -->
+
+      <header>
+         <div class="title"> 
+            <label> <a style="text-decoration: none; color:#6a5acd"  href="index.php" title="Home"> TinyTypers </a> </label>
+         </div>
+      </header>
+
+      <!-- MAIN AREA -->
+      <div class="main">
+         <div class="mainTitle">
+            <label for="main" class="login"> LOGIN </label>
+            <label for="main" class="register"> REGISTER </label>
+         </div>
+
+         <form action="" method="POST" class="loginForm" name="loginForm">
+
+            <ul>
+               <li class="loginUser"> Username or Email <br>
+                  <input type="text" class="loginUserInp" name="loginUserOrEmail" required>
+               </li>
+
+               <li class="loginPass"> Password <br>
+                  <input type="password" class="loginPassInp" name="loginPass" required>
+               </li>
+
+               <li class="loginSubmit">
+                  <input type="submit" class="loginButton" value="Login" name="loginButton">
+               </li>
+            </ul>
+         </form>
+
+         <form action="" method="POST" class="registerForm" name="registerForm">
+            <ul class="mainInfo">
+               <li class="regUser"> Username <br>
+                  <input type="text" class="regUserInp" name="regUser" required>
+               </li>
+
+               <li class="regEmail"> Email <br>
+                  <input type="text" class="regEmailInp" name="regEmail" required>
+               </li>
+
+               <li class="regPass"> Password <br>
+                  <input type="password" class="regPassInp" name="regPass" required>
+               </li>
+            </ul>
+
+            <ul class="secondaryInfo">
+               <li class="fName"> First Name <br>
+                  <input type="text" class="regFNameInp" name="regFName" required>
+               </li>
+
+               <li class="lName"> Last Name <br>
+                  <input type="text" class="regLNameInp" name="regLName" required>
+               </li>
+
+               <li class="regSubmit">
+                  <input type="submit" class="regButton" value="Register" name="registerButton">
+               </li>
+            </ul>
+         
+         </form>
+      </div>
+
+      <!-- FOOTER -->
+
+      <footer>
+         <nav>
+            <ul>
+               <li> 1 </li>
+               <li> 2 </li>
+               <li> 3 </li>
+               <li> 4 </li>
+               <li> 5 </li>
+            </ul>
+         </nav>
+      </footer>
+
+   </div>
+
+</body>
+
+</html>
