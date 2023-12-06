2ff26ee2f59d49d1f33a4c29414a32de503b30bc
T <witiwtyler@gmail.com>
2023-12-05 16:41:17 -0800

    started main php/sql work

--- profile.php
+++ profile.php
@@ -1,9 +1,51 @@
 <?php
+session_start();
+// Login creds
+$loginUsernameOrEmail = filter_input(INPUT_POST, 'loginUserOrEmail');
+$loginPassword = filter_input(INPUT_POST, 'loginPass');
+// Register creds
+$regUsername = filter_input(INPUT_POST, 'regUser');
+$regEmail = filter_input(INPUT_POST, 'regEmail');
+$regPassword = filter_input(INPUT_POST, 'regPass');
+$regFName = filter_input(INPUT_POST, 'regFName');
+$regLName = filter_input(INPUT_POST, 'regLName');
 
-// $db = mysqli_connect("69.172.204.200", "tp", "New_Web_2023!", "tp_db");
-// if (!$db) {
-//    die("Cannot connect to database! Error: " . mysqli_connect_error());
-// }
+
+$db = mysqli_connect("69.172.204.200", "tp", "New_Web_2023!", "tp_Tiny_Typers");
+if (!$db) {
+   die("Cannot connect to database! Error: ".mysqli_connect_error());
+}
+
+// If user is logging in
+if (isset($_POST['login']) && !isset($_POST['register'])) {
+   $loginQuery = "SELECT password FROM user WHERE username = '$loginUsernameOrEmail' OR email = '$loginUsernameOrEmail'";
+
+   $queryResult = mysqli_query($db, $loginQuery);
+
+   if ($queryResult == '' || $queryResult == null) {
+      die("Invalid Username, Email, or Password entered. Try again!");
+   }
+
+   $row = mysqli_fetch_assoc($queryResult);
+   $fetchedPassword = $row["password"];
+
+
+   if (password_verify($password, $fetchedPassword)) {
+      header("Location: index.php/");
+   }
+}
+
+// If user is registering
+if (isset($_POST['register']) && !isset($_POST['login'])) {
+   $regUserQuery = "INSERT INTO user (username, email, password) VALUES ('$regUsername', '$regEmail', '$regPassword')";
+   $regUserDetQuery = "INSERT INTO user_det (f_Name, l_name) VALUES ('$regFName', '$regLName')";
+
+   $userQueryResult = mysqli_query($db, $regUserQuery);
+   $userDetQueryResult = mysqli_query($db, $regUserDetQuery);
+   
+}
+
+
 
 ?>
 
@@ -30,58 +72,58 @@
       </header>
 
       <!-- MAIN AREA -->
-      <form action="" class="main">
+      <div class="main">
          <div class="mainTitle">
             <label for="main" class="login"> LOGIN </label>
             <label for="main" class="register"> REGISTER </label>
          </div>
 
-         <div class="loginForm">
+         <form action="" class="loginForm" name="loginForm">
             <ul>
                <li class="loginUser"> Username or Email <br>
-                  <input type="text" class="loginUserInp">
+                  <input type="text" class="loginUserInp" name="loginUserOrEmail" required>
                </li>
 
                <li class="loginPass"> Password <br>
-                  <input type="password" class="loginPassInp">
+                  <input type="password" class="loginPassInp" name="loginPass" required>
                </li>
 
                <li class="loginSubmit">
-                  <input type="button" class="loginButton" value="Login">
+                  <input type="button" class="loginButton" value="Login" name="login">
                </li>
             </ul>
-         </div>
+         </form>
 
-         <div class="registerForm">
+         <form action="" class="registerForm" name="registerForm">
             <ul class="mainInfo">
                <li class="regUser"> Username<br>
-                  <input type="text" class="regUserInp">
+                  <input type="text" class="regUserInp" name="regUser" required>
                </li>
 
                <li class="regEmail"> Email <br>
-                  <input type="text" class="regEmailInp">
+                  <input type="text" class="regEmailInp" name="regEmail" required>
                </li>
 
                <li class="regPass"> Password <br>
-                  <input type="password" class="regPassInp">
+                  <input type="password" class="regPassInp" name="regPass" required>
                </li>
             </ul>
 
             <ul class="secondaryInfo">
                <li class="fName"> First Name <br>
-                  <input type="text" class="regFNameInp">
+                  <input type="text" class="regFNameInp" name="regFName" required>
                </li>
 
                <li class="lName"> Last Name <br>
-                  <input type="text" class="regLNameInp">
+                  <input type="text" class="regLNameInp" name="regLName" required>
                </li>
 
                <li class="regSubmit">
-                  <input type="button" class="regButton" value="Register">
+                  <input type="button" class="regButton" value="Register" name="register">
                </li>
             </ul>
-         </div>
-      </form>
+         </form>
+      </div>
 
       <!-- FOOTER -->
 
