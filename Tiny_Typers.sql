1b26265b9a300c8916847e5fc9acd38655a9b1bb
Blaze <balazsm8519@gmail.com>
2023-12-07 11:21:35 -0800

    Tiny_Typers.sql

--- Tiny_Typers.sql
+++ Tiny_Typers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 29, 2023 at 12:53 PM
+-- Generation Time: Dec 07, 2023 at 11:20 AM
 -- Server version: 8.0.34-0ubuntu0.20.04.1
 -- PHP Version: 7.4.3-4ubuntu2.19
 
@@ -29,9 +29,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `attempt` (
-  `run_id` int NOT NULL,
-  `user_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  `id` int NOT NULL,
+  `run_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attempt_wpm`
+--
+
+CREATE TABLE `attempt_wpm` (
+  `attempt_id` int NOT NULL,
+  `wpm_id` int NOT NULL,
+  `wpm` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -41,8 +53,8 @@ CREATE TABLE `attempt` (
 
 CREATE TABLE `friends_list` (
   `user_id` int NOT NULL,
-  `friend's_code` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  `friend_code` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -53,22 +65,21 @@ CREATE TABLE `friends_list` (
 CREATE TABLE `run` (
   `id` int NOT NULL,
   `user_id` int NOT NULL,
-  `words` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `time` decimal(2,0) NOT NULL,
   `key` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `run_wpm`
+-- Table structure for table `run_words`
 --
 
-CREATE TABLE `run_wpm` (
-  `attempt_id` int NOT NULL,
-  `wpm_id` int NOT NULL,
-  `wpm` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+CREATE TABLE `run_words` (
+  `run_id` int NOT NULL,
+  `index` int NOT NULL DEFAULT '0',
+  `words` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -79,10 +90,10 @@ CREATE TABLE `run_wpm` (
 CREATE TABLE `user` (
   `id` int NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `passoword` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `username` varchar(50) NOT NULL,
   `friend_code` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -95,8 +106,8 @@ CREATE TABLE `user_det` (
   `theme` int NOT NULL DEFAULT '1',
   `f_name` varchar(50) NOT NULL,
   `l_name` varchar(100) NOT NULL,
-  `pfp` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT 'Default.jpg'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  `pfp` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'Default.jpg'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Indexes for dumped tables
@@ -106,13 +117,19 @@ CREATE TABLE `user_det` (
 -- Indexes for table `attempt`
 --
 ALTER TABLE `attempt`
-  ADD PRIMARY KEY (`run_id`);
+  ADD PRIMARY KEY (`id`,`run_id`);
+
+--
+-- Indexes for table `attempt_wpm`
+--
+ALTER TABLE `attempt_wpm`
+  ADD PRIMARY KEY (`attempt_id`,`wpm_id`);
 
 --
 -- Indexes for table `friends_list`
 --
 ALTER TABLE `friends_list`
-  ADD PRIMARY KEY (`user_id`,`friend's_code`);
+  ADD PRIMARY KEY (`user_id`,`friend_code`);
 
 --
 -- Indexes for table `run`
@@ -122,16 +139,17 @@ ALTER TABLE `run`
   ADD UNIQUE KEY `key` (`key`);
 
 --
--- Indexes for table `run_wpm`
+-- Indexes for table `run_words`
 --
-ALTER TABLE `run_wpm`
-  ADD PRIMARY KEY (`attempt_id`,`wpm_id`);
+ALTER TABLE `run_words`
+  ADD PRIMARY KEY (`run_id`,`index`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `friend_code` (`friend_code`);
 
 --
 -- Indexes for table `user_det`
