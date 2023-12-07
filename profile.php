858848ad27e3fa54af1b66cccf065245bbcd5f81
Grimm <witwityler@gmail.com>
2023-12-06 22:31:21 -0800

    failed to get logout to work, also added footer page links

--- profile.php
+++ profile.php
@@ -2,8 +2,6 @@
 
 <?php
 
-
-
 ?>
 
 <!-- HTML START -->
@@ -23,38 +21,41 @@
 
       <header>
          <div class="title">
-            <label> <a style="text-decoration: none; color:#6a5acd"  href="index.php" title="Home"> TinyTypers </a> </label>
+            <label> <a style="text-decoration: none; color:#6a5acd" href="index.php" title="Home"> TinyTypers </a>
+            </label>
          </div>
       </header>
 
       <!-- MAIN AREA -->
 
-      <div class="main">
-         <div class="profile">
-            <label for="profile"> YOUR NAME </label>
+      <form action="" method="POST" class="profileForm" name="profileForm">
+         <div class="main">
+            <div class="profile">
+               <label for="profile"> YOUR NAME </label>
+               <label> <?php echo $loggedInStatus; ?> </label>
+               <input type="submit" class="logOutButton" name="logOutButton" value="Log Out">
+            </div>
+
+            <!-- <label> Log Out </label> -->
 
          </div>
-
-
-      </div>
+      </form>
 
       <!-- FOOTER -->
 
       <footer>
          <nav>
             <ul>
-               <li> 1 </li>
-               <li> 2 </li>
-               <li> 3 </li>
-               <li> 4 </li>
-               <li> 5 </li>
+               <li> <a style="text-decoration: none; color:#6a5acd" href="extraSites/contactUs.html" title="Contact Us"> Contact Us </a> </li>
+               <li> <a style="text-decoration: none; color:#6a5acd" href="extraSites/cookieUsage.html" title="Cookie Usage"> Cookie Usage </a> </li>
+               <li> <a style="text-decoration: none; color:#6a5acd" href="extraSites/ourLocation.html" title="Our Location"> Our Location </a> </li>
+               <li> <a style="text-decoration: none; color:#6a5acd" href="extraSites/aboutUs.html" title="About Us"> About Us </a> </li>
+               <li> <a style="text-decoration: none; color:#6a5acd" href="index.php" title="TinyTypers"> ©TinyTypers </a> </li>
             </ul>
          </nav>
-
       </footer>
-      
-   </div>
 
+   </div>
 
 </body>
 
